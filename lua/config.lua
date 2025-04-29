bb0444d6938cd45143eb1048c2383e1788fcf826
Hippasusss <dannyherbertgj@gmail.com>
2025-04-29 23:07:28 +0100

    added config

---
+++ lua/config.lua
@@ -0,0 +1,13 @@
+M = {}
+local default_options = {
+    register = "",
+}
+
+M.options = {}
+
+function M.setAllOptions(opts)
+    M.options = vim.tbl_deep_extend("force", default_options, opts or {})
+    return M.options
+end
+
+return M
