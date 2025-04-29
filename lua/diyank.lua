4e545bbd68c219b0e0b23ae7ca3cd00e985cf344
Hippasusss <dannyherbertgj@gmail.com>
2025-04-29 22:44:06 +0100

    first working version

--- lua/diyank.lua
+++ lua/diyank.lua
@@ -8,7 +8,28 @@
 
 M = {}
 
-function M.copyDiagnostic()
+function M.copyDiagnosticFromCurrentLine()
+
+    local lineNum = vim.fn.line('.') - 1;
+
+    local diags = vim.diagnostic.get(0, {})
+    if not diags or #diags == 0 then
+        print("No diagnostics")
+        return
+    end
+
+    local message = ''
+    for _, diagnostic in ipairs(diags) do
+        if diagnostic.lnum == lineNum then
+            message = message .. diagnostic.message
+        end
+    end
+
+    vim.fn.setreg("+", message)
+    print("Copied diagnostics to clipboard.")
+end
+
+function M.setup()
 end
 
 return M
